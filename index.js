b228e5da5c0d97dc111b4c52a0e69db7a36f3291
d-g-town <66391417+d-g-town@users.noreply.github.com>
2024-10-16 13:16:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
     res.send('Tetris time.');
 });
 
-var sponsor = process.env.SPONSOR || "unsponsoredd";
+var sponsor = process.env.SPONSOR || "unsponsored";
 app.listen(3000, () => console.log('Tetris app is listening on port 3000! Brought to you by the one & only', sponsor + ", of course."));
